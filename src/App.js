f750ed5cc09f6473f2ef03484d6ca89e4cbdd9a1
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-10-29 13:42:44 +0600

    ReducerCounter using useReducer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ReducerCounter from './components/ReducerCounter/ReducerCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ReducerCounter></ReducerCounter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ReducerCounter/ReducerCounter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReducerCounter = () => {
+    return (
+        <div>
+            <h1>reducer counter</h1>
+        </div>
+    );
+};
+
+export default ReducerCounter;
