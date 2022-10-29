d26fecbe3fd5ec5c204ff6adb162aea769b68e07
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-10-29 14:45:59 +0600

    Reducer counter updated

--- src/components/ReducerCounter/ReducerCounter.js
+++ src/components/ReducerCounter/ReducerCounter.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import './ReducerCounter.css';
 
-const ReducerCounter = () => {
+function init(initialCount) {
+    return {count: initialCount};
+  }
+
+function reducer(state, action){
+    switch(action.type){
+        case 'increment':
+            return{count: state.count + 1};
+        case 'decrement':
+            return {count: state.count - 1};
+        case 'reset':
+            return init(action.payload);
+        default:
+            throw new Error();
+    }
+}
+
+const ReducerCounter = ({initialCount}) => {
+    const [state, dispatch] = useReducer(reducer, initialCount, init);
+
     return (
         <div>
-            <h1>reducer counter</h1>
+            <h2> Count : {state.count}</h2>
+            <br />
+            <button
+                onClick={() => dispatch({type: 'reset', payload: 0})}>
+                Reset
+            </button>
+            <button onClick={ () => dispatch({type: 'decrement'})}>-</button>
+            <button onClick={ () => dispatch({type: 'increment'})}>+</button>
         </div>
     );
 };
